b7ee9e5e4658be53a62299564ee6172cfe225eb1
Bajura Andrei <slayer2004@list.ru>
2025-02-19 12:51:15 +0300

    something

---
+++ progone/include/Buffer.h
@@ -0,0 +1,76 @@
+
+
+#ifndef BUFFER_H
+
+#define BUFFER_H
+
+#include <condition_variable>
+#include <vector>
+#include <thread>
+
+template <typename T> class Buffer {
+  std::condition_variable cv_;
+  std::vector<T> buf_;
+
+  mutable std::mutex mut_;
+
+public:
+  void add(const T &data);
+
+  std::condition_variable &get_cv();
+
+  void clear();
+  T back() const;
+
+  bool pop(T &ref);
+
+  int size() const;
+  bool empty() const;
+};
+
+
+template <typename T> void Buffer<T>::add(const T &data) {
+  {
+    std::lock_guard lk(mut_);
+    buf_.push_back(data);
+  }
+  cv_.notify_one();
+}
+
+template <class T> std::condition_variable &Buffer<T>::get_cv() { return cv_; }
+
+template <class T> void Buffer<T>::clear() {
+  std::lock_guard lk{mut_};
+  buf_.clear();
+}
+
+template <class T> T Buffer<T>::back() const {
+  std::lock_guard lk{mut_};
+  T data = buf_.back();
+  return data;
+}
+
+template <class T> bool Buffer<T>::pop(T &ref) {
+  std::lock_guard lk(mut_);
+  if (buf_.empty()) {
+    return false;
+  }
+  ref = std::move(buf_.back());
+  buf_.pop_back();
+  return true;
+}
+
+
+template <class T> bool Buffer<T>::empty() const {
+    std::lock_guard lk{mut_};
+    return buf_.empty();
+  }
+
+template <class T> int Buffer<T>::size() const {
+  std::lock_guard lk{mut_};
+  return buf_.size();
+}
+
+
+
+#endif
---
+++ progone/src/main.cpp
@@ -0,0 +1,100 @@
+#include "Buffer.h"
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cctype>
+#include <iostream>
+#include <mutex>
+#include <ranges>
+#include <thread>
+
+constexpr int alp_size = 52;
+
+struct CharCount {
+  char letter;
+  int count;
+
+  CharCount(char letter, int count) : letter(letter), count(count) {}
+
+  CharCount(const CharCount &o) = default;
+  CharCount(CharCount &&o) noexcept = default;
+};
+
+std::ostream &operator<<(std::ostream &str, const CharCount &d) {
+  str << d.letter << " " << d.count << "\n";
+  return str;
+}
+
+using data_type = std::vector<CharCount>;
+
+data_type count_entries(const auto &str) {
+  std::array<int, alp_size> din{};
+  for (const char c : str) {
+    int ind = c >= 'a' ? c - 'a' : c - 'A' + alp_size / 2;
+    ++din[ind];
+  }
+  std::vector<CharCount> vec;
+  for (int i = 0; i < alp_size / 2; ++i) {
+    if (din[i] > 0) {
+      vec.emplace_back(static_cast<char>('a' + i), din[i]);
+    }
+  }
+  for (int i = alp_size / 2; i < alp_size; ++i) {
+    if (din[i] > 0) {
+      vec.emplace_back(static_cast<char>('A' + i - alp_size / 2), din[i]);
+    }
+  }
+  return vec;
+}
+
+void thread_one(Buffer<data_type> &buf) {
+  namespace rng = std::ranges;
+
+  for (;;) {
+    std::string input;
+    std::cin >> input;
+
+    auto is_letter = [](char c) {
+      return std::isalpha(static_cast<unsigned char>(c));
+    };
+    if (input.size() > 64) {
+      std::cerr << "Wrong input. Length of input sequence is greater than 64 "
+                   "symbols\n";
+      continue;
+    }
+    if (!rng::all_of(input, is_letter)) {
+      std::cerr
+          << "Wrong input. Input sequence contains non-alphabetic letters\n";
+      continue;
+    }
+
+    auto entries = count_entries(input);
+    buf.add(entries);
+  }
+}
+
+void thread_two(Buffer<data_type> &buf) {
+  std::mutex mut;
+  for (;;) {
+    auto &cv = buf.get_cv();
+    std::unique_lock lk(mut);
+    cv.wait(lk, [&buf] { return buf.size() > 0; });
+
+    data_type data;
+    if (buf.empty() || !buf.pop(data)) {
+      continue;
+    }
+
+    for (const auto &d : data) {
+      std::cout << d;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  Buffer<data_type> buf;
+  std::jthread one(thread_one, std::ref(buf));
+  std::jthread two(thread_two, std::ref(buf));
+
+  return 0;
+}
